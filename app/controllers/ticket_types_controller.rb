501a8a2e9f5878d32dae81ed51b01ad46eae23ae
phuonghoang713 <phuonghoang713@gmail.com>
2017-07-01 12:29:16 +0700

    complete create ticket types

--- app/controllers/ticket_types_controller.rb
+++ app/controllers/ticket_types_controller.rb
@@ -1,12 +1,11 @@
 class TicketTypesController < ApplicationController
+  before_action :current_event
   def new
-    @event = Event.find(params[:event_id])
-    puts "event_id params: #{params[:event_id]}"
     @ticket_type = TicketType.new
   end
 
   def create
-    @ticket_type = TicketType.create(ticket_type_params)
+    @ticket_type = @event.ticket_types.create(ticket_type_params)
     if @ticket_type.save!
       flash[:success] = "Created a new event"
       redirect_to root_path
@@ -15,6 +14,12 @@ class TicketTypesController < ApplicationController
       render 'new'
     end
   end
+
+  def current_event
+    @event = Event.find(params[:event_id])
+    puts "event_id params: #{params[:event_id]}"
+  end
+  private
   def ticket_type_params
     params.require(:ticket_type).permit(:price, :name, :max_quantity)
   end
